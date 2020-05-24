83428df654d5e8b48ac5594c24cc80dad4ee0521
Ed L <el-github@elangley.org>
2020-05-23 20:39:54 -0700

    update do.sh

--- do.sh
+++ do.sh
@@ -4,3 +4,4 @@ docker run --name paper_builder paper_builder:latest
 docker cp paper_builder:/process/paper/proc/paper.tex .
 docker cp paper_builder:/process/paper/paper.pdf .
 docker rm paper_builder
+tar zcf release.tgz paper.tex paper.pdf paper.md paper.bib preamble
